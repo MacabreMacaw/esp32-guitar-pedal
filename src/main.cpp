16af1b0d9e5368b2c13b5f18cc200821994c0c6f
Will <willtritsch7@gmail.com>
2026-01-26 13:39:07 -0500

    Add toggle and bypass

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 
+constexpr int BUTTON_PIN = 4;
 constexpr int LED_PIN = 2;
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
+bool lastButtonState = HIGH;
+bool bypass = true;
+
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  Serial.println("LED ON");
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  bool currentButtonState = digitalRead(BUTTON_PIN);
+
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
+    bypass = !bypass;
+    Serial.println(bypass ? "BYPASS" : "EFFECT ON");
+    digitalWrite(LED_PIN, !bypass);
+  }
+
+  lastButtonState = currentButtonState;
 }
